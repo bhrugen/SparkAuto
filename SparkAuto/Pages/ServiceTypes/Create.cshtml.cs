1cfb248bb39fb9e4c1a7395668b2383ab174dab8
Ben Spark <bhrugen1990@gmail.com>
2019-01-13 22:01:56 -0600

    Section 5 - Create Get Handler and Razor Page

---
+++ SparkAuto/Pages/ServiceTypes/Create.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SparkAuto.Models;
+
+namespace SparkAuto.Pages.ServiceTypes
+{
+    public class CreateModel : PageModel
+    {
+
+        public ServiceType ServiceType { get; set; }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+    }
+}
