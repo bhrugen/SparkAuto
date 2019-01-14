b5c7d8af236f972a378006ac9f519ffa3fbde37c
Ben Spark <bhrugen1990@gmail.com>
2019-01-14 16:48:07 -0600

    Section 7 - Paging Info Class

---
+++ SparkAuto/Models/PagingInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SparkAuto.Models
+{
+    public class PagingInfo
+    {
+        public int TotalItems { get; set; }
+        public int ItemsPerPage { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalPage => (int)Math.Ceiling((decimal)TotalItems / ItemsPerPage);
+
+        public string UrlParam { get; set; }
+    }
+}
