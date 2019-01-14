241c0bbbaa175018af04947a9e11a4deeaae05e6
Ben Spark <bhrugen1990@gmail.com>
2019-01-13 22:52:38 -0600

    Assignment 2 - Service Type Delete Razor Page

---
+++ SparkAuto/Pages/ServiceTypes/Delete.cshtml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SparkAuto.Data;
+using SparkAuto.Models;
+
+namespace SparkAuto.Pages.ServiceTypes
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public DeleteModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public ServiceType ServiceType { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            ServiceType = await _db.ServiceType.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (ServiceType == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (ServiceType == null)
+            {
+                return NotFound();
+            }
+
+            _db.ServiceType.Remove(ServiceType);
+            await _db.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
