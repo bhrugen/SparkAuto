b4b28e93fc0f064a424769e4bbe241f6b52d98d7
Ben Spark <bhrugen1990@gmail.com>
2019-01-13 23:55:10 -0600

    Section 7 - User Index Get Handler

---
+++ SparkAuto/Pages/Users/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SparkAuto.Data;
+using SparkAuto.Models;
+
+namespace SparkAuto.Pages.Users
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public List<ApplicationUser> ApplicationUserList { get; set; }
+
+        public async Task<IActionResult> OnGet()
+        {
+            ApplicationUserList = await _db.ApplicationUser.ToListAsync();
+            return Page();
+        }
+    }
+}
