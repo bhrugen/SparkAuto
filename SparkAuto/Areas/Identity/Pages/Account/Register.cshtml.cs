1c38bf2bbd7df56f808c62e3d7068b50fe311310
Ben Spark <bhrugen1990@gmail.com>
2019-01-13 23:14:08 -0600

    Section 6 - Register Razor Page UI

--- SparkAuto/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ SparkAuto/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -54,6 +54,15 @@ namespace SparkAuto.Areas.Identity.Pages.Account
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
+
+            [Required]
+            public string Name { get; set; }
+            public string Address { get; set; }
+            public string City { get; set; }
+            public string PostalCode { get; set; }
+
+            [Required]
+            public string PhoneNumber { get; set; }
         }
 
         public void OnGet(string returnUrl = null)
