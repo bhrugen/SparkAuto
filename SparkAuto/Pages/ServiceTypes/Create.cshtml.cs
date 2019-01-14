262dfd1495ab9b9c9b07eef90ba9ce70dddd1432
Ben Spark <bhrugen1990@gmail.com>
2019-01-13 22:16:16 -0600

    Section 5 - Create Post Handler and Validations

--- SparkAuto/Pages/ServiceTypes/Create.cshtml.cs
+++ SparkAuto/Pages/ServiceTypes/Create.cshtml.cs
@@ -4,18 +4,41 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SparkAuto.Data;
 using SparkAuto.Models;
 
 namespace SparkAuto.Pages.ServiceTypes
 {
     public class CreateModel : PageModel
     {
+        private readonly ApplicationDbContext _db;
 
+        [BindProperty]
         public ServiceType ServiceType { get; set; }
+
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
 
         public IActionResult OnGet()
         {
             return Page();
         }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if(!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _db.ServiceType.Add(ServiceType);
+            await _db.SaveChangesAsync();
+
+            return RedirectToPage("Index");
+        }
+
     }
 }
