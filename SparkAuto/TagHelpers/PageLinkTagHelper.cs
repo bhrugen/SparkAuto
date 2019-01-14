26dfbfcc6a536802ddf2a9483828cc97d49bd1a2
Ben Spark <bhrugen1990@gmail.com>
2019-01-14 16:58:19 -0600

    Section 7 - Page Link Custom Tag Helper Part 1

---
+++ SparkAuto/TagHelpers/PageLinkTagHelper.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using SparkAuto.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SparkAuto.TagHelpers
+{
+    [HtmlTargetElement("div",Attributes ="page-model")]
+    public class PageLinkTagHelper : TagHelper
+    {
+        private IUrlHelperFactory urlHelperFactory;
+
+        public PageLinkTagHelper(IUrlHelperFactory helperfactory)
+        {
+            urlHelperFactory = helperfactory;
+        }
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
+
+        public PagingInfo pageModel { get; set; }
+        public string PageAction { get; set; }
+        public string PageClass { get; set; }
+        public string PageClassNormal { get; set; }
+        public string PageClassSelected { get; set; }
+
+
+    }
+}
