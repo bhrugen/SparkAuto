96723cf30b8a2b67c173edea82a96518cc779f9f
Ben Spark <bhrugen1990@gmail.com>
2019-01-13 22:48:59 -0600

    Assignment 1 - Service Type Details Razor Page

---
+++ SparkAuto/Pages/ServiceTypes/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SparkAuto.Data;
+using SparkAuto.Models;
+
+namespace SparkAuto.Pages.ServiceTypes
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public DetailsModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public ServiceType ServiceType { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            ServiceType = await _db.ServiceType.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (ServiceType == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
