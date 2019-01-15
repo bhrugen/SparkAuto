bc1501b18a2c76de398f472aee9ca023821f5c14
Ben Spark <bhrugen1990@gmail.com>
2019-01-14 20:55:46 -0600

    Assignment 3 - Users Edit Razor Page

---
+++ SparkAuto/Pages/Users/Edit.cshtml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SparkAuto.Data;
+using SparkAuto.Models;
+using SparkAuto.Utility;
+
+namespace SparkAuto.Pages.Users
+{
+    [Authorize(Roles = SD.AdminEndUser)]
+    public class EditModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public ApplicationUser ApplicationUser { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(string id)
+        {
+            if (id.Trim().Length == 0)
+            {
+                return NotFound();
+            }
+
+            ApplicationUser = await _db.ApplicationUser.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (ApplicationUser == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            else
+            {
+                var userInDb = await _db.ApplicationUser.SingleOrDefaultAsync(u => u.Id == ApplicationUser.Id);
+                if (userInDb == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    userInDb.Name = ApplicationUser.Name;
+                    userInDb.PhoneNumber = ApplicationUser.PhoneNumber;
+                    userInDb.Address = ApplicationUser.Address;
+                    userInDb.City = ApplicationUser.City;
+                    userInDb.PostalCode = ApplicationUser.PostalCode;
+
+                    await _db.SaveChangesAsync();
+                    return RedirectToPage("Index");
+                }
+            }
+        }
+
+
+    }
+}
