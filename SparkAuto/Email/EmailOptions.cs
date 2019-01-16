f129edbcf3042b53f9bb88e2061d9b722de2c3e7
Ben Spark <bhrugen1990@gmail.com>
2019-01-15 22:49:34 -0600

    Section 8 - Send Emails Part 1

---
+++ SparkAuto/Email/EmailOptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SparkAuto.Email
+{
+    public class EmailOptions
+    {
+        public string SendGridKey { get; set; }
+    }
+}
