9bd82fc0bbff7b06863eb7e4f24c73ad65f82583
Ben Spark <bhrugen1990@gmail.com>
2019-01-16 23:31:10 -0600

    Section 9 - Car Create Razor Page

---
+++ SparkAuto/Pages/Cars/Create.cshtml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SparkAuto.Data;
+using SparkAuto.Models;
+
+namespace SparkAuto.Pages.Cars
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        [BindProperty]
+        public Car Car { get; set; }
+
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult OnGet(string userId=null)
+        {
+            Car = new Car();
+            if (userId == null)
+            {
+                var claimsIdentity = (ClaimsIdentity)User.Identity;
+                var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+                userId = claim.Value;
+            }
+            Car.UserId = userId;
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            //if (!ModelState.IsValid)
+            //{
+            //    return Page();
+            //}
+
+            //_db.ServiceType.Add(ServiceType);
+            //await _db.SaveChangesAsync();
+
+            return RedirectToPage("Index");
+        }
+
+    }
+}
