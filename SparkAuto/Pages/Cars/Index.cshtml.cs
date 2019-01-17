a1b9e8bd28980d6d1c2d48999640ca60807ee007
Ben Spark <bhrugen1990@gmail.com>
2019-01-16 23:04:11 -0600

    Section 9 - Car Index Get Hanndler

---
+++ SparkAuto/Pages/Cars/Index.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SparkAuto.Data;
+using SparkAuto.Models.ViewModel;
+
+namespace SparkAuto.Pages.Cars
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+
+        [BindProperty]
+        public CarAndCustomerViewModel CarAndCustVM { get; set; }
+
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IActionResult> OnGet(string userId = null)
+        {
+            if(userId==null)
+            {
+                var claimsIdentity = (ClaimsIdentity)User.Identity;
+                var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+                userId = claim.Value;
+            }
+
+
+            CarAndCustVM = new CarAndCustomerViewModel()
+            {
+                Cars = await _db.Car.Where(c => c.UserId == userId).ToListAsync(),
+                UserObj = await _db.ApplicationUser.FirstOrDefaultAsync(u => u.Id == userId)
+            };
+
+            return Page();
+        }
+    }
+}
