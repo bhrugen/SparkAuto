095f08b19f84c9f9e9de154623b550d332a99f29
Ben Spark <bhrugen1990@gmail.com>
2019-01-16 22:56:35 -0600

    Section 9 - Car and Customer VM

---
+++ SparkAuto/Models/ViewModel/CarAndCustomerViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SparkAuto.Models.ViewModel
+{
+    public class CarAndCustomerViewModel
+    {
+        public ApplicationUser UserObj { get; set; }
+        public IEnumerable<Car> Cars { get; set; }
+    }
+}
