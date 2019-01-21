f8d99ff00b11ba687d51810d8ccec5ee1f1da252
Ben Spark <bhrugen1990@gmail.com>
2019-01-21 15:28:08 -0600

    Section 10 - Shopping Cart View Model

---
+++ SparkAuto/Models/ServcieShoppingCart.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SparkAuto.Models
+{
+    public class ServcieShoppingCart
+    {
+        public int Id { get; set; }
+        public int CarId { get; set; }
+        public int ServiceTypeId { get; set; }
+
+        [ForeignKey("CarId")]
+        public virtual Car Car { get; set; }
+
+        [ForeignKey("ServiceTypeId")]
+        public virtual ServiceType ServiceType { get; set; }
+    }
+}
