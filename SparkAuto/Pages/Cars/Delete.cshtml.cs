0a93865afaa79e9aebc50d6fbb2ed497b41ba084
Ben Spark <bhrugen1990@gmail.com>
2019-01-21 11:34:48 -0600

    Assignment 6 - Cars Delete Razor Page

---
+++ SparkAuto/Pages/Cars/Delete.cshtml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SparkAuto.Data;
+using SparkAuto.Models;
+
+namespace SparkAuto.Pages.Cars
+{
+    [Authorize]
+    public class DeleteModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        public DeleteModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public Car Car { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Car = await _db.Car
+                .Include(c => c.ApplicationUser).FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Car == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (Car == null)
+            {
+                return NotFound();
+            }
+            var userId = Car.UserId;
+
+            _db.Car.Remove(Car);
+            await _db.SaveChangesAsync();
+            StatusMessage = "Car deleted successfully.";
+            return RedirectToPage("./Index", new { userId });
+        }
+    }
+}
