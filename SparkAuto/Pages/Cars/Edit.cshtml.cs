90f96be267d95dbfe12384895a06345e3c6436c4
Ben Spark <bhrugen1990@gmail.com>
2019-01-21 11:32:40 -0600

    Assignment 5 - Cars Edit Razor Page

---
+++ SparkAuto/Pages/Cars/Edit.cshtml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SparkAuto.Data;
+using SparkAuto.Models;
+
+namespace SparkAuto.Pages.Cars
+{
+    [Authorize]
+    public class EditModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public Car Car { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Car = await _db.Car
+                .Include(c => c.ApplicationUser).FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Car == null)
+            {
+                return NotFound();
+            }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _db.Attach(Car).State = EntityState.Modified;
+
+            await _db.SaveChangesAsync();
+            StatusMessage = "Car updated successfully.";
+            return RedirectToPage("./Index", new { userId = Car.UserId });
+        }
+
+
+    }
+}
