50691bfdc1a6adaaad131d1675f92b38ca0479fe
Ben Spark <bhrugen1990@gmail.com>
2019-01-21 15:42:18 -0600

    Section 10 - Service Header and Details Model

---
+++ SparkAuto/Models/ServiceDetails.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SparkAuto.Models
+{
+    public class ServiceDetails
+    {
+        public int Id { get; set; }
+
+        public int ServiceHeaderId { get; set; }
+        [ForeignKey("ServiceHeaderId")]
+        public virtual ServiceHeader ServiceHeader { get; set; }
+
+        [Display(Name ="Service")]
+        public int ServiceTypeId { get; set; }
+        [ForeignKey("ServiceTypeId")]
+        public virtual ServiceType ServiceType { get; set; }
+
+        public double ServicePrice { get; set; }
+        public string ServiceName { get; set; }
+    }
+}
---
+++ SparkAuto/Models/ServiceHeader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SparkAuto.Models
+{
+    public class ServiceHeader
+    {
+        public int Id { get; set; }
+        public double Miles { get; set; }
+        [Required]
+        public double TotalPrice { get; set; }
+        public string Details { get; set; }
+
+        [Required]
+        [DisplayFormat(DataFormatString ="{0:MMM dd yyyy}")]
+        public DateTime DateAdded { get; set; }
+
+        public  int CarId { get; set; }
+
+        [ForeignKey("CarId")]
+        public virtual Car Car { get; set; }
+    }
+}
