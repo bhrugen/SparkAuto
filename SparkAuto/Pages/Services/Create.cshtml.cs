c1fea0e34d8035bb8be37f29f590de0de8b36a24
Ben Spark <bhrugen1990@gmail.com>
2019-01-21 22:19:34 -0600

    Section 10 - Create Service Get Handler

---
+++ SparkAuto/Pages/Services/Create.cshtml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SparkAuto.Data;
+using SparkAuto.Models;
+using SparkAuto.Models.ViewModel;
+using SparkAuto.Utility;
+
+namespace SparkAuto.Pages.Services
+{
+    [Authorize(Roles =SD.AdminEndUser)]
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        [BindProperty]
+        public CarServiceViewModel CarServiceVM { get; set; }
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IActionResult> OnGet(int carId)
+        {
+            CarServiceVM = new CarServiceViewModel
+            {
+                Car = await _db.Car.Include(c => c.ApplicationUser).FirstOrDefaultAsync(c => c.Id == carId),
+                ServiceHeader = new Models.ServiceHeader()
+            };
+
+            List<String> lstServiceTypeInShoppingCart = _db.ServiceShoppingCart
+                                                            .Include(c => c.ServiceType)
+                                                            .Where(c => c.CarId == carId)
+                                                            .Select(c => c.ServiceType.Name)
+                                                            .ToList();
+
+            IQueryable<ServiceType> lstService = from s in _db.ServiceType
+                                                 where !(lstServiceTypeInShoppingCart.Contains(s.Name))
+                                                 select s;
+
+            CarServiceVM.ServiceTypesList = lstService.ToList();
+
+            CarServiceVM.ServiceShoppingCart = _db.ServiceShoppingCart.Include(c => c.ServiceType).Where(c => c.CarId == carId).ToList();
+            CarServiceVM.ServiceHeader.TotalPrice = 0;
+
+            foreach(var item in CarServiceVM.ServiceShoppingCart)
+            {
+                CarServiceVM.ServiceHeader.TotalPrice += item.ServiceType.Price;
+            }
+
+            return Page();
+
+        }
+    }
+}
