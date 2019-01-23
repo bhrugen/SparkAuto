266eae72b923bcae45a2d701b99e1abc15067dd5
Ben Spark <bhrugen1990@gmail.com>
2019-01-22 23:49:37 -0600

    Section 10 - Service Details Get Handler

---
+++ SparkAuto/Pages/Services/Details.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SparkAuto.Data;
+using SparkAuto.Models;
+
+namespace SparkAuto.Pages.Services
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public DetailsModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public ServiceHeader serviceHeader { get; set; }
+        public List<ServiceDetails> serviceDetails { get; set; }
+
+        public void OnGet(int serviceId)
+        {
+            serviceHeader = _db.ServiceHeader.Include(s => s.Car).Include(s => s.Car.ApplicationUser).FirstOrDefault(s => s.Id == serviceId);
+            serviceDetails = _db.ServiceDetails.Where(s => s.ServiceHeaderId == serviceId).ToList();
+        }
+    }
+}
