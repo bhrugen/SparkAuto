c35195c2b8e10fbf8a30ebd36e4259b8fdbe987f
Ben Spark <bhrugen1990@gmail.com>
2019-01-22 23:13:42 -0600

    Section 10 - Service History Get Handler

---
+++ SparkAuto/Pages/Services/History.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SparkAuto.Data;
+using SparkAuto.Models;
+
+namespace SparkAuto.Pages.Services
+{
+    public class HistoryModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public HistoryModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public List<ServiceHeader> ServiceHeader { get; set; }
+
+        public async Task OnGet(int carId)
+        {
+            ServiceHeader = await _db.ServiceHeader.Include(s => s.Car).Include(c => c.Car.ApplicationUser).Where(c => c.CarId == carId).ToListAsync();
+        }
+    }
+}
