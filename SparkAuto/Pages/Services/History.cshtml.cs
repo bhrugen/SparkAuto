1115982408a1c4a6823f1e3367040e5b2e7b1ceb
Ben Spark <bhrugen1990@gmail.com>
2019-01-22 23:24:10 -0600

    Section 10 - Service History Razor Page

--- SparkAuto/Pages/Services/History.cshtml.cs
+++ SparkAuto/Pages/Services/History.cshtml.cs
@@ -22,9 +22,13 @@ namespace SparkAuto.Pages.Services
         [BindProperty]
         public List<ServiceHeader> ServiceHeader { get; set; }
 
+        public string UserId { get; set; }
+
         public async Task OnGet(int carId)
         {
             ServiceHeader = await _db.ServiceHeader.Include(s => s.Car).Include(c => c.Car.ApplicationUser).Where(c => c.CarId == carId).ToListAsync();
+
+            UserId = _db.Car.Where(u => u.Id == carId).ToList().FirstOrDefault().UserId;
         }
     }
 }
