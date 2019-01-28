5de0c828fe7231a22dc99d0650fd8dd8d795f019
Ben Spark <bhrugen1990@gmail.com>
2019-01-28 12:18:24 -0600

    Section 11 - Redirect on Login

--- SparkAuto/Pages/Index.cshtml.cs
+++ SparkAuto/Pages/Index.cshtml.cs
@@ -1,17 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SparkAuto.Utility;
 
 namespace SparkAuto.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        public IActionResult OnGet()
         {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
 
+            if(claim==null)
+            {
+                return RedirectToPage("/Account/Login", new { area = "Identity" });
+            }
+
+            if(User.IsInRole(SD.AdminEndUser))
+            {
+                return RedirectToPage("/Users/Index");
+            }
+            return RedirectToPage("/Cars/Index");
         }
     }
 }
