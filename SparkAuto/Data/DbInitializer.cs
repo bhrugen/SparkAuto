9890e0dc4806d60f5db051ee4c822c4f81df4964
Ben Spark <bhrugen1990@gmail.com>
2019-01-31 15:55:48 -0600

    Section 12 - Seed Database Part 1

---
+++ SparkAuto/Data/DbInitializer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SparkAuto.Data
+{
+    public class DbInitializer : IDbInitializer
+    {
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
