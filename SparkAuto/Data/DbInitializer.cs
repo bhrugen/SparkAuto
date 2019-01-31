687e4860b94bb1cf014a70fc850fba151eb39faf
Ben Spark <bhrugen1990@gmail.com>
2019-01-31 16:07:20 -0600

    Section 12 - Seed Database Part 2

--- SparkAuto/Data/DbInitializer.cs
+++ SparkAuto/Data/DbInitializer.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SparkAuto.Models;
+using SparkAuto.Utility;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,9 +11,51 @@ namespace SparkAuto.Data
 {
     public class DbInitializer : IDbInitializer
     {
-        public void Initialize()
+
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public DbInitializer(ApplicationDbContext db, UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
         {
-            throw new NotImplementedException();
+            _db = db;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+
+        public async void Initialize()
+        {
+            try
+            {
+                if(_db.Database.GetPendingMigrations().Count()>0)
+                {
+                    _db.Database.Migrate();
+                }
+            }
+            catch(Exception ex)
+            {
+
+            }
+
+            if (_db.Roles.Any(r => r.Name == SD.AdminEndUser)) return;
+
+            _roleManager.CreateAsync(new IdentityRole(SD.AdminEndUser)).GetAwaiter().GetResult();
+            _roleManager.CreateAsync(new IdentityRole(SD.CustomerEndUser)).GetAwaiter().GetResult();
+
+            _userManager.CreateAsync(new ApplicationUser
+            {
+                UserName = "admin@gmail.com",
+                Email = "admin@gmail.com",
+                Name = "Ben Spark",
+                EmailConfirmed = true,
+                PhoneNumber = "1112223333"
+            }, "Admin123*").GetAwaiter().GetResult();
+
+            IdentityUser user = await _db.Users.FirstOrDefaultAsync(u => u.Email == "admin@gmail.com");
+
+            _userManager.AddToRoleAsync(user, SD.AdminEndUser).GetAwaiter().GetResult();
+
         }
     }
 }
